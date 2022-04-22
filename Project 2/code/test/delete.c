b9e24031c0f364b300e66fc0d5358f95dd8e690c
bingsuxoaii <ngtphuongtrang67@gmail.com>
2022-04-22 19:06:09 +0700

    finish all

---
+++ Project 2/code/test/delete.c
@@ -0,0 +1,23 @@
+#include "../userprog/syscall.h"
+
+int main()
+{
+    int namesize;
+    char source[MAX_STR_LEN];
+
+    PrintString("\n[DELETE FILE]",15);
+    PrintString("\n> Enter file's name size: ",27);
+    namesize = ReadNum();
+
+    PrintString("\n> Enter file name: ",21);
+    ReadString(source, namesize);
+
+    if (Remove(source))
+    {
+        PrintString("=> Deleted successfully!", 25);
+    }
+    else
+        PrintString("=> Deleted unsuccessfully!", 29);
+
+    Halt();
+}
